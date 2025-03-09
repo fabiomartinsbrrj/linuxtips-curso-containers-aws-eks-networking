8e452f016a92a5311ddd37eeef834a3e36ff8ff5
fabiomartinsbrrj <fabiomartinsbr@gmail.com>
2025-03-08 21:25:07 -0300

    feat: Creating Subnets

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "CIDR principal da VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type = list(string)
+  description = "Lista de CIDRS adicionais da VPC"
+  default = []
+}
+
+variable "public_subnets" {
+  description = "Lista de Public Subnets da VPC"
+  type = list(object({
+    name = string
+    cidr = string
+    availability_zone = string
+  }))
+}
